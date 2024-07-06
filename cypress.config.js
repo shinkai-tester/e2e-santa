e68059bf925f5072a7879e2491f092bdd86cbf44
Aleksandra Klimantova <alexandra.klimantowa@gmail.com>
2024-07-06 03:42:20 +0200

    [HW_6_1_1] refactoring, e2e test is fixed

--- cypress.config.js
+++ cypress.config.js
@@ -1,8 +1,9 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  watchForFileChanges: false,
   e2e: {
-    baseUrl: "https://staging.lpitko.ru",
+    baseUrl: "https://santa-secret.ru",
     testIsolation: false,
     setupNodeEvents(on, config) {
       // implement node event listeners here
